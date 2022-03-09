64e7934e880ed357095acea4788de6690454d8f5
kkyznechikk <ann9014017@gmail.com>
2022-03-09 12:42:57 +0300

    feat: timer

---
+++ script.js
@@ -0,0 +1,18 @@
+let date = new Date('Dec 31 2022 00:00:00');
+function counts (){
+    let now = new Date();
+    let gap = date - now;
+    console.log(gap)
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60 ) % 24;
+    let minutes = Math.floor(gap / 1000 / 60 ) % 60;
+    let seconds = Math.floor(gap / 1000 ) % 60;
+    
+    document.getElementById('d').innerText = days;
+    document.getElementById('h').innerText = hours;
+    document.getElementById('m').innerText = minutes;
+    document.getElementById('s').innerText = seconds;
+}
+counts();
+
+setInterval(counts, 1000);
